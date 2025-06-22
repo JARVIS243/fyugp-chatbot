61033a95c2ad98d40aeecfba4c8bf7b581a6d168
JARVIS243 <jarvisai0025@gmail.com>
2025-06-22 09:53:35 +0530

    app.py

--- app.py
+++ app.py
@@ -189,11 +189,11 @@ with col2:
         else:
             answer = duckduckgo_answer(user_input)
 
-        bot_box = st.empty()
-        bot_box.markdown(f"<div class='message bot'>{answer}</div>", unsafe_allow_html=True)
-        st.session_state.chat_history.append(("bot", answer))
-        st.rerun()
-
+        if st.button("🔁 Reset Chat"):
+            for key in ["chat_history", "pdf_text", "greeted"]:
+                if key in st.session_state:
+                    del st.session_state[key]
+            st.experimental_rerun()
 
     st.markdown("<div style='text-align:center; color:#666; margin-top: 30px;'>© 2025 | Published by Aju Krishna</div>", unsafe_allow_html=True)
     st.markdown("</div>", unsafe_allow_html=True)
