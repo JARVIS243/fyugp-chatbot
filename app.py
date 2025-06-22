d9c713bb63a580af48bc8c88cad6f6955094c9d9
JARVIS243 <jarvisai0025@gmail.com>
2025-06-22 10:47:55 +0530

    app.py

--- app.py
+++ app.py
@@ -51,8 +51,8 @@ st.markdown("""
     backdrop-filter: blur(8px);
     background-color: rgba(255, 255, 255, 0.05);
     border-radius: 15px;
-    min-height: 85vh;
-    max-height: 85vh;
+    min-height: 8vh;
+    max-height: 18vh;
     overflow-y: auto;
 }
 
