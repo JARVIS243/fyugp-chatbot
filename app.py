54e12de9195c0860982d4c0a0409ec565ae82583
JARVIS243 <jarvisai0025@gmail.com>
2025-06-22 10:31:07 +0530

    app.py

--- app.py
+++ app.py
@@ -15,9 +15,6 @@ if "chat_history" not in st.session_state:
 if "pdf_text" not in st.session_state:
     st.session_state.pdf_text = ""
 
-if "greeted" not in st.session_state:
-    st.session_state.greeted = False
-
 # --- Get Internet Date and Time ---
 def get_internet_datetime():
     try:
@@ -135,11 +132,7 @@ with col2:
     st.markdown("<div class='content'>", unsafe_allow_html=True)
     st.markdown("<div class='title'>FYUGP Assistant</div>", unsafe_allow_html=True)
     st.markdown("<div class='subtitle'>Ask from PDF, Useful websites, or any doubt, Nb:- Use Small Letters for get the answers</div>", unsafe_allow_html=True)
-    st.markdown(f"<div style='text-align:right; color:#888;'>🗓️ {get_internet_datetime()}</div>", unsafe_allow_html=True)
-
-    if not st.session_state.greeted:
-        st.success("👋 Welcome to FYUGP Assistant! Ask your questions or upload a PDF to begin.")
-        st.session_state.greeted = True
+    st.markdown(f"<div style='text-align:right; color:#888;'> {get_internet_datetime()}</div>", unsafe_allow_html=True)
 
     for role, msg in st.session_state.chat_history:
         css_class = "user" if role == "user" else "bot"
